0c5ec3b31d18994fd8ea173151bfa640fa379b56
guguan123 <guguanming@outlook.com>
2025-01-25 20:32:50 +0800

    格式化代码

--- 开心农场/sys/fnc/add_farm_event.php
+++ 开心农场/sys/fnc/add_farm_event.php
@@ -1,5 +1,5 @@
 <?php
 function add_farm_event($msg) {
 	global $user;
-	dbquery("INSERT INTO `farm_event` (`time`, `uid`, `msg`) VALUES ('".time()."', '".$user['id']."', '".my_esc($msg)."')");
+	dbquery("INSERT INTO `farm_event` (`time`, `uid`, `msg`) VALUES ('" . time() . "', '" . $user['id'] . "', '" . my_esc($msg) . "')");
 }
