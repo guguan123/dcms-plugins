7022df8992277830bbda36db4c34443753e988e2
guguan123 <guguanming@outlook.com>
2025-01-26 20:22:35 +0800

    格式化代码

--- 开心农场/farm/ambar.php
+++ 开心农场/farm/ambar.php
@@ -18,20 +18,20 @@ if (isset($_GET['id'])) $int = intval($_GET['id']);
 $fuser = dbarray(dbquery("SELECT * FROM `farm_user` WHERE `uid` = '" . $user['id'] . "' LIMIT 1"));
 
 if (isset($_GET['sell_all'])) {
-    dbquery("DELETE FROM `farm_ambar` WHERE `id_user`= '" . $user['id'] . "'");
-    $sumd = intval($_SESSION['sum']);
-    dbquery("UPDATE `farm_user` SET `gold` = '" . ($fuser['gold'] + $sumd) . "' WHERE `uid` = '" . $user['id'] . "' LIMIT 1");
-    add_farm_event('所有的庄稼都卖了 ' . intval($_SESSION['sum']) . ' 黄金');
-    unset($_SESSION['sum']);
+	dbquery("DELETE FROM `farm_ambar` WHERE `id_user`= '" . $user['id'] . "'");
+	$sumd = intval($_SESSION['sum']);
+	dbquery("UPDATE `farm_user` SET `gold` = '" . ($fuser['gold'] + $sumd) . "' WHERE `uid` = '" . $user['id'] . "' LIMIT 1");
+	add_farm_event('所有的庄稼都卖了 ' . intval($_SESSION['sum']) . ' 黄金');
+	unset($_SESSION['sum']);
 
-    header("Location: /plugins/farm/ambar");
-    exit;
+	header("Location: /plugins/farm/ambar");
+	exit;
 }
 
 if (isset($_GET['sell_ok'])) {
-    $semen = dbarray(dbquery("select * from `farm_plant` WHERE  `id` = '" . intval($_SESSION['plid']) . "'  LIMIT 1"));
+	$semen = dbarray(dbquery("select * from `farm_plant` WHERE  `id` = '" . intval($_SESSION['plid']) . "'  LIMIT 1"));
 
-    add_farm_event('植物产量 ' . $semen['name'] . ' 卖了。收到 ' . intval($_SESSION['dohod']) . ' 黄金.');
+	add_farm_event('植物产量 ' . $semen['name'] . ' 卖了。收到 ' . intval($_SESSION['dohod']) . ' 黄金.');
 }
 
 aut();
@@ -39,87 +39,87 @@ aut();
 include 'inc/str.php';
 
 if (isset($_GET['id'])) {
-    $post = dbarray(dbquery("select * from `farm_ambar` WHERE `id`= '" . intval($_GET['id']) . "' LIMIT 1"));
-    $semen = dbarray(dbquery("select * from `farm_plant` WHERE  `id` = '" . $post['semen'] . "'  LIMIT 1"));
+	$post = dbarray(dbquery("select * from `farm_ambar` WHERE `id`= '" . intval($_GET['id']) . "' LIMIT 1"));
+	$semen = dbarray(dbquery("select * from `farm_plant` WHERE  `id` = '" . $post['semen'] . "'  LIMIT 1"));
 
-    unset($_SESSION['plid']);
+	unset($_SESSION['plid']);
 
-    $_SESSION['plid'] = $post['semen'];
+	$_SESSION['plid'] = $post['semen'];
 
-    $dohod = $post['kol'] * $semen['dohod'];
+	$dohod = $post['kol'] * $semen['dohod'];
 
 
-    unset($_SESSION['dohod']);
-    $_SESSION['dohod'] = $dohod;
-    if (isset($_GET['sell'])) {
-        $dohod = intval($_SESSION['dohod']);
-        dbquery("UPDATE `farm_user` SET `gold` = '" . ($fuser['gold'] + $dohod) . "' WHERE `uid` = '" . $user['id'] . "' LIMIT 1");
-        dbquery("DELETE FROM `farm_ambar` WHERE `id` = " . intval($_GET['id']) . " ");
-        header('Location: /plugins/farm/ambar?sell_ok');
-    }
+	unset($_SESSION['dohod']);
+	$_SESSION['dohod'] = $dohod;
+	if (isset($_GET['sell'])) {
+		$dohod = intval($_SESSION['dohod']);
+		dbquery("UPDATE `farm_user` SET `gold` = '" . ($fuser['gold'] + $dohod) . "' WHERE `uid` = '" . $user['id'] . "' LIMIT 1");
+		dbquery("DELETE FROM `farm_ambar` WHERE `id` = " . intval($_GET['id']) . " ");
+		header('Location: /plugins/farm/ambar?sell_ok');
+	}
 
-    farm_event();
+	farm_event();
 
-    echo "<img src='/plugins/farm/bush/" . $post['semen'] . ".png' alt=''><br/> <b>" . $semen['name'] . "</b><br/>";
-    echo "&raquo; 收获量: <b>" . $post['kol'] . "</b> <br/>";
-    echo "&raquo; 单价: <b>" . $semen['dohod'] . "</b> <br/>";
+	echo "<img src='/plugins/farm/bush/" . $post['semen'] . ".png' alt=''><br/> <b>" . $semen['name'] . "</b><br/>";
+	echo "&raquo; 收获量: <b>" . $post['kol'] . "</b> <br/>";
+	echo "&raquo; 单价: <b>" . $semen['dohod'] . "</b> <br/>";
 
-    echo "&raquo; 总收入: <b>" . $dohod . "</b> <br/>";
+	echo "&raquo; 总收入: <b>" . $dohod . "</b> <br/>";
 
-    echo "<form method='post' action='?id=" . $int . "&amp;sell'>\n";
-    echo "<input type='submit' name='save' value='出售' />\n";
-    echo "</form>\n";
-    echo "<div class='rowup'>";
-    echo "&laquo; <a href='/plugins/farm/ambar'>仓库</a><br/>";
-    echo "</div>";
+	echo "<form method='post' action='?id=" . $int . "&amp;sell'>\n";
+	echo "<input type='submit' name='save' value='出售' />\n";
+	echo "</form>\n";
+	echo "<div class='rowup'>";
+	echo "&laquo; <a href='/plugins/farm/ambar'>仓库</a><br/>";
+	echo "</div>";
 } else {
 
-    farm_event();
+	farm_event();
 
-    $k_post = dbresult(dbquery("SELECT COUNT(*) FROM `farm_ambar` WHERE `id_user` = '" . $user['id'] . "'"), 0);
-    $k_page = k_page($k_post, $set['p_str']);
-    $page = page($k_page);
-    $start = $set['p_str'] * $page - $set['p_str'];
-
-
-    if ($k_post == 0) {
-        echo "<div class='err'>仓库里没有货物</div>";
-    }
-
-    if ($k_post != 0) {
-        $rssum = dbquery("SELECT * FROM `farm_ambar` WHERE `id_user` = '" . $user['id'] . "'");
-        $_SESSION['sum'] = 0;
-
-        while ($item = dbarray($rssum)) {
-            $plsum = dbarray(dbquery("SELECT * FROM `farm_plant` WHERE `id` = '" . $item['semen'] . "' LIMIT 1"));
-            $plussum = $plsum['dohod'] * $item['kol'];
-            $_SESSION['sum'] = $plussum + $_SESSION['sum'];
-        }
-
-        echo "<div class='rowdown'>";
-        echo "<img src='/plugins/farm/img/add.png' alt='' class='rpg' /> <a href='/plugins/farm/ambar?sell_all'>把一切都卖了 " . intval($_SESSION['sum']) . " 黄金</a></div>";
-    }
+	$k_post = dbresult(dbquery("SELECT COUNT(*) FROM `farm_ambar` WHERE `id_user` = '" . $user['id'] . "'"), 0);
+	$k_page = k_page($k_post, $set['p_str']);
+	$page = page($k_page);
+	$start = $set['p_str'] * $page - $set['p_str'];
 
 
-    $res = dbquery("select * from `farm_ambar` WHERE `id_user` = '" . $user['id'] . "' LIMIT $start, $set[p_str];");
+	if ($k_post == 0) {
+		echo "<div class='err'>仓库里没有货物</div>";
+	}
 
-    while ($post = dbarray($res)) {
-        if ($num == 1) {
-            echo "<div class='rowdown'>";
-            $num = 0;
-        } else {
-            echo "<div class='rowup'>";
-            $num = 1;
-        }
+	if ($k_post != 0) {
+		$rssum = dbquery("SELECT * FROM `farm_ambar` WHERE `id_user` = '" . $user['id'] . "'");
+		$_SESSION['sum'] = 0;
 
-        $semen = dbarray(dbquery("select * from `farm_plant` WHERE  `id` = '" . $post['semen'] . "'  LIMIT 1"));
+		while ($item = dbarray($rssum)) {
+			$plsum = dbarray(dbquery("SELECT * FROM `farm_plant` WHERE `id` = '" . $item['semen'] . "' LIMIT 1"));
+			$plussum = $plsum['dohod'] * $item['kol'];
+			$_SESSION['sum'] = $plussum + $_SESSION['sum'];
+		}
 
-        echo "<img src='/plugins/farm/bush/" . $post['semen'] . ".png' height='20' width='20'><b></b> <a href='?id=$post[id]'>" . $semen['name'] . "</a> [" . $post['kol'] . "] ";
-        echo "(<a href='?id=" . $post['id'] . "&amp;sell'><span class='off'>出售</span></a>)</div>";
-    }
+		echo "<div class='rowdown'>";
+		echo "<img src='/plugins/farm/img/add.png' alt='' class='rpg' /> <a href='/plugins/farm/ambar?sell_all'>把一切都卖了 " . intval($_SESSION['sum']) . " 黄金</a></div>";
+	}
 
 
-    if ($k_page > 1) str('?', $k_page, $page); // Вывод страниц
+	$res = dbquery("select * from `farm_ambar` WHERE `id_user` = '" . $user['id'] . "' LIMIT $start, $set[p_str];");
+
+	while ($post = dbarray($res)) {
+		if ($num == 1) {
+			echo "<div class='rowdown'>";
+			$num = 0;
+		} else {
+			echo "<div class='rowup'>";
+			$num = 1;
+		}
+
+		$semen = dbarray(dbquery("select * from `farm_plant` WHERE  `id` = '" . $post['semen'] . "'  LIMIT 1"));
+
+		echo "<img src='/plugins/farm/bush/" . $post['semen'] . ".png' height='20' width='20'><b></b> <a href='?id=$post[id]'>" . $semen['name'] . "</a> [" . $post['kol'] . "] ";
+		echo "(<a href='?id=" . $post['id'] . "&amp;sell'><span class='off'>出售</span></a>)</div>";
+	}
+
+
+	if ($k_page > 1) str('?', $k_page, $page); // Вывод страниц
 }
 echo "<div class='rowdown'>";
 echo "<img src='/plugins/farm/img/garden.png' /> <a href='/plugins/farm/garden/'>返回</a><br/>";
